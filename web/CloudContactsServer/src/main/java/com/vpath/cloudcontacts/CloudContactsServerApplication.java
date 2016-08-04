6c6131508e9b4e491979a2bb2a598d971ff254cb
nag <nagpach@gmail.com>
2016-08-04 17:56:56 -0400

    Built Basic Backend

---
+++ web/CloudContactsServer/src/main/java/com/vpath/cloudcontacts/CloudContactsServerApplication.java
@@ -0,0 +1,26 @@
+package com.vpath.cloudcontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class CloudContactsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudContactsServerApplication.class, args);
+	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //registry.addMapping("/greeting").allowedOrigins("http://192.168.1.135:9000");
+            }
+        };
+    }
+}
